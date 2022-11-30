2f54756c251638bc0b96a94af90a93ae3d0965d4
Elizabeth <mwendemuthusi6@gmail.com>
2022-11-30 21:34:32 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,9 @@
+let input;
+
 function addingEventListener() {
+    input = document.getElementById('input');
+    input.addEventListener('click', ()=> alert('I was clicked!'));
+    
 }
+console.log(addingEventListener())
+
